6619bf4826cecb1d17ec0470d5437bcbb8e30b9e
S W <xgithub@willcock.com>
2022-09-20 21:39:21 +0100

    added random left and right

--- main.ts
+++ main.ts
@@ -10,14 +10,21 @@ basic.forever(function () {
     if (u < 20) {
         basic.showIcon(IconNames.No)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         music.playMelody("G F G A - F E D ", 500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
         basic.pause(500)
         music.stopAllSounds()
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        if (Math.randomBoolean()) {
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        } else {
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        }
         basic.pause(500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
