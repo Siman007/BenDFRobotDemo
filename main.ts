d7635404044542624ee7b6fc29eb35659ccfe136
S W <xgithub@willcock.com>
2022-09-20 21:33:57 +0100

    fixed issues

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 let u = 0
 basic.clearScreen()
-basic.showString("Hello!")
+basic.showString("3")
+basic.showNumber(2)
+basic.showNumber(1)
 basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
     u = maqueen.Ultrasonic(PingUnit.Centimeters)
-    basic.showNumber(u)
-    if (u < 15) {
+    if (u < 20) {
+        basic.showIcon(IconNames.No)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         music.playMelody("G F G A - F E D ", 500)
@@ -15,14 +18,14 @@ basic.forever(function () {
         music.stopAllSounds()
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.pause(200)
+        basic.pause(500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        basic.clearScreen()
     } else {
     	
     }
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    basic.pause(100)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    basic.pause(500)
 })
