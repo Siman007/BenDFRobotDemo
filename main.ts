ce0ddc94390a3cd3770bb2258da196ec7ba10c7d
S W <xgithub@willcock.com>
2022-09-20 21:23:06 +0100

    fixed bug

--- main.ts
+++ main.ts
@@ -6,16 +6,17 @@ basic.clearScreen()
 basic.forever(function () {
     u = maqueen.Ultrasonic(PingUnit.Centimeters)
     basic.showNumber(u)
-    if (u < 10) {
+    if (u < 15) {
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
         maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         music.playMelody("G F G A - F E D ", 500)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
-        basic.pause(1000)
+        basic.pause(500)
         music.stopAllSounds()
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.pause(1000)
+        basic.pause(200)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
         maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
     } else {
@@ -23,4 +24,5 @@ basic.forever(function () {
     }
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     basic.pause(100)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
 })
