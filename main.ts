5fa48b928510251ca63ddceb0af9d7c070031140
S W <xgithub@willcock.com>
2022-09-20 21:16:57 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+let u = 0
+basic.clearScreen()
+basic.showString("Hello!")
+basic.pause(2000)
+basic.clearScreen()
+basic.forever(function () {
+    u = maqueen.Ultrasonic(PingUnit.Centimeters)
+    basic.showNumber(u)
+    if (u < 10) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        music.playMelody("G F G A - F E D ", 500)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        basic.pause(1000)
+        music.stopAllSounds()
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(1000)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    } else {
+    	
+    }
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    basic.pause(100)
+})
